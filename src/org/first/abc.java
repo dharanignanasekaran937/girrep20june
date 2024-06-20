6dd104c2a5c8f2d1ea71f9bf14c9a42635bb7c39
dharanignanasekaran937 <dharanignansekaran937@gmail.com>
2024-06-20 16:54:22 +0530

    maths

---
+++ src/org/first/abc.java
@@ -0,0 +1,20 @@
+package org.first;
+
+public class abc {
+	private void green() {
+		System.out.println("this is a first project");
+	}
+	private void brown() {
+		System.out.println("brown is method name");
+
+	}
+	
+	public static void main(String[] args) {
+	
+		abc a = new abc();
+		a.green();
+		a.brown();
+	}
+
+}
+
